21c1c68261e25f125a7abffef902b30a45c7b6e1
masterajima <98704309+masterajima@users.noreply.github.com>
2023-06-13 11:00:54 -0400

    backend

--- react-tailwind-app/public/db/dbConnect.js
+++ react-tailwind-app/public/db/dbConnect.js
@@ -1,4 +1,10 @@
 import mongoose from "mongoose";
 require("dotenv").config();
-async function dbConnect() {}
+async function dbConnect() {
+  mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex,
+  });
+}
 module.export = dbConnect;
