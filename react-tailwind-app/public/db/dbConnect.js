58862a363c82759b15aa18ccd57ecb20465d6a66
masterajima <98704309+masterajima@users.noreply.github.com>
2023-06-13 11:11:59 -0400

    modify connection

--- react-tailwind-app/public/db/dbConnect.js
+++ react-tailwind-app/public/db/dbConnect.js
@@ -1,13 +1,23 @@
-import mongoose from "mongoose";
-
-//use mongoose to connect app to database
+// external imports
+const mongoose = require("mongoose");
 require("dotenv").config();
+
 async function dbConnect() {
-  mongoose.connect(process.env.DB_URL, {
-    //to ensure connection establish properly
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  });
+  // use mongoose to connect this app to our database on mongoDB using the DB_URL (connection string)
+  mongoose
+    .connect(process.env.DB_URL, {
+      //   these are options to ensure that the connection is done properly
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
+    .then(() => {
+      console.log("Successfully connected to MongoDB Atlas!");
+    })
+    .catch((error) => {
+      console.log("Unable to connect to MongoDB Atlas!");
+      console.error(error);
+    });
 }
-module.export = dbConnect;
+
+module.exports = dbConnect;
