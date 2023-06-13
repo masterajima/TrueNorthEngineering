159939cbcfb9eef785fc971ba1401c0c1f7e9f6e
masterajima <98704309+masterajima@users.noreply.github.com>
2023-06-13 11:18:28 -0400

    require database connection

--- truenorth/src/App.js
+++ truenorth/src/App.js
@@ -6,6 +6,10 @@ import Login2 from "./components/Login2";
 import Navbar from "./components/Navbar";
 
 function App() {
+  //require database connection
+  const dbConnect = require("./db/dbConnect");
+
+  //execute database connection
   return (
     <>
       <Navbar />
